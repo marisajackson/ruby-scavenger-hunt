4764815807f4333b954d4f0080bd555d6a6a0366
MaRisa Jackson <mhljackson@gmail.com>
2014-07-22 11:49:53 -0500

    fixed distance, added pathlength

---
+++ lib/MyVector.rb
@@ -0,0 +1,22 @@
+require 'matrix'
+
+class MyVector < Vector
+
+  def distance(v)
+    Math.sqrt((self[0] - v[0])**2 + (self[1] - v[1])**2)
+  end
+
+  def self.pathlength(*v)
+    length = 0
+    0.upto(v.length - 2) do |i|
+      v1 = v[i]
+      v2 = v[i + 1]
+      distance = Math.sqrt((v1[0] - v2[0])**2 + (v1[1] - v2[1])**2)
+      puts distance
+      length += distance
+      puts length
+    end
+    length
+  end
+
+end
